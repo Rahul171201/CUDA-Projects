5f43b3ddeb3913d35f88bd53720efb37ab339160
Rahul Roy <rahulroydps@gmail.com>
2022-10-18 12:31:06 +0530

    updated project folder and code

--- image_conversion/kernel.cu
+++ image_conversion/kernel.cu
@@ -123,9 +123,7 @@ int main()
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
-        {
             cout << greyscale_image[i * N + j] << " ";
-        }
         cout << "\n";
     }
 
